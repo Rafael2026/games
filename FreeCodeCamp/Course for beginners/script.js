fef6b728f5e34b416ca009ad9ef10298c4392f48
Rafael2026 <rafaaguilarmunoz@gmail.com>
2023-05-07 18:21:03 +0200

    Many changes and new game

---
+++ FreeCodeCamp/Course for beginners/script.js
@@ -0,0 +1,40 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+let x = 0;
+const playerImage = new Image();
+const spriteWidth = 6876/12;
+const spriteHeight = 5230/10;
+let frameX = 0;
+let frameY = 5;
+let gameFrame = 0;
+let staggerFrames = 5;
+
+playerImage.src = 'img/shadowDog.png';
+canvas.width = 600;
+canvas.height = 600;
+
+function animate() {
+  
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  //ctx.fillRect(50, 50, 100, 100);
+  //ctx.drawImage(playerImage, 50, 50);
+
+  let position = Math.floor(gameFrame/staggerFrames) % 6;
+
+  ctx.drawImage(playerImage, (frameX * spriteWidth), (frameY * spriteHeight), spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+  
+  /*if (gameFrame%staggerFrames == 0) {
+
+    if (frameX < 4) {
+      frameX++;
+    } else {
+      frameX = 0;
+    }
+  }*/
+
+  gameFrame++;
+  requestAnimationFrame(animate);
+};
+
+animate();
